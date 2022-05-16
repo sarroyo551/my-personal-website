aa3c81eb44cb8cf5cafa028db8e2cb7bbb5a6611
Santi Arroyo <santi.arroyo551@gmail.com>
2022-05-16 11:06:46 -0400

    created image folder and updated html

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2);*/
+
+
 
