9d105c4c0e6d3357b2d65a0ee3a04fdcbda0ec08
Santi Arroyo <santi.arroyo551@gmail.com>
2022-05-16 11:55:10 -0400

    started javascript project

--- script.js
+++ script.js
@@ -1,7 +1,73 @@
-/*const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);*/
+document.addEventListener('DOMContentLoaded'), () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'art',
+            img: 'images/art.jpeg'
+        },
+        {
+            name: 'art',
+            img: 'images/art.jpeg'
+        },
+        {
+            name: 'black-space',
+            img: 'images/black_space.png'
+        },
+        {
+            name: 'black-space',
+            img: 'images/black_space.png'
+        },
+        {
+            name: 'blank',
+            img: 'images/blank.png'
+        },
+        {
+            name: 'blank',
+            img: 'images/blank.png'
+        },
+        {
+            name: 'boat',
+            img: 'images/boat.jpeg'
+        },
+        {
+            name: 'boat',
+            img: 'images/boat.jpeg'
+        },
+        {
+            name: 'camera',
+            img: 'images/camera.jpeg'
+        },
+        {
+            name: 'camera',
+            img: 'images/camera.jpeg'
+        },
+        {
+            name: 'camera2',
+            img: 'images/camera2.jpeg'
+        },
+        {
+            name: 'camera2',
+            img: 'images/camera2.jpeg'
+        },
+        {
+            name: 'guitar',
+            img: 'images/guitar.jpeg'
+        },
+        {
+            name: 'guitar',
+            img: 'images/guitar.jpeg'
+        },
+        {
+            name: 'seville'
+            img: 'images/seville.jpeg'
+        },
+        {
+            name: 'seville'
+            img: 'images/seville.jpeg'
+        },
+    ]
+const grid = document.querySelector.apply('.grid')
+//create your board
 
 
 
